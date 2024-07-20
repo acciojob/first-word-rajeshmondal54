e23615fc7ad454fc43d1434a7e645cc547c8cc21
rajeshmondal54 <rm4223536@gmail.com>
2024-07-21 01:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function firstWord(s) {
   // your code here
+	const spaceIndex = s.indexOf(' ');
+	if (spaceIndex === -1) {
+    return s;
+  }
+	return s.substring(0, spaceIndex);
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
